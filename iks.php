e1e275b2278187753e2ddf476a2719be0e69ecfc
Ram Chandra Limbu <ramchandralimbu75@gmail.com>
2023-02-15 10:51:34 +0545

    New Commit

--- iks.php
+++ iks.php
@@ -8,3 +8,23 @@ echo "The time is " . date("h:i:sa");
 
 </body>
 </html>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "The time <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
+
+</body>
+</html>
+is " . date("h:i:sa");
+?>
+
+</body>
+</html>
